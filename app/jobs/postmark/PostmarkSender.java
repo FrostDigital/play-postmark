b5452be2fff604f983c49c29dfe36cd3babae050
joelso <joel.soderstrom@gmail.com>
2011-12-04 15:53:18 +0100

    wip

---
+++ app/jobs/postmark/PostmarkSender.java
@@ -0,0 +1,114 @@
+package jobs.postmark;
+
+import com.postmark.java.PostmarkClient;
+import com.postmark.java.PostmarkException;
+import com.postmark.java.PostmarkMessage;
+import com.postmark.java.PostmarkResponse;
+
+import play.Logger;
+import play.Play;
+import play.i18n.Messages;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+import play.modules.postmark.Postmark;
+import play.test.Fixtures;
+
+public class PostmarkSender extends Job {
+	
+	/**
+	 * Client that communicates with Postmark's REST API.
+	 */
+	private static PostmarkClient postmarkClient;
+	
+	/**
+	 * The message *this* sender will send
+	 */
+	private PostmarkMessage message;
+	
+	/**
+	 * Private, use {@link #sendMail(...)} instead!
+	 * @param message
+	 */
+	private PostmarkSender(PostmarkMessage message) {
+		this.message = message;
+	}
+	
+    public void doJob() {
+    	try {
+    		if(Postmark.ctx.isTest()) {
+    			logMessage(message);
+    		}
+    		PostmarkResponse resp = postmarkClient.sendMessage(message);
+    		
+    		if(Postmark.ctx.isTest()) {
+    			logResponse(resp);
+    		}
+    	} catch (PostmarkException e) {
+    		Logger.warn("Could not send mail to '%s':", message.getToAddress(), e.getMessage());
+    	}
+    }
+    
+    /**
+     * Send mail given recipient from given sender.
+     * @param from - sender, must be a valid "sender signature" registered @ postmarkapp.com
+     * @param to
+     * @param subject
+     * @param body - message content
+     */
+    public static void sendMail(String from, String to, String subject, String body) {
+    	if(from == null && Postmark.ctx.getDefaultFrom() != null) {
+    		from = Postmark.ctx.getDefaultFrom();
+    		assert from != null : "Cannot send mail, 'from' is null";
+    	}
+    	sendMail(new PostmarkMessage(from, to, null, null, subject, body, false, null));
+    }
+    
+    /**
+     * Send mail to given recipient.
+     * @param to
+     * @param subject
+     * @param body - message content
+     */
+    public static void sendMail(String to, String subject, String body) {
+    	sendMail(null, to, subject, body);
+    }
+    
+    /**
+     * Creates a new AsyncMailJob and runs it.
+     * @param message
+     * @param isTest
+     */
+    public static void sendMail(PostmarkMessage message) {
+    	assert Postmark.ctx != null : "Postmark module has not been initialized";
+
+    	if(postmarkClient == null) {
+    		postmarkClient = new PostmarkClient(Postmark.ctx.getApiKey());
+    	}
+    	new PostmarkSender(message).doJob();
+    }
+    
+    
+	private static void logMessage(PostmarkMessage message) {
+		Logger.info(
+			new StringBuilder()
+			.append("\n------- Postmark will send message ------")
+			.append("\nTo: ").append(message.getToAddress())
+			.append("\nFrom: ").append(message.getFromAddress())
+			.append("\nSubject: ").append(message.getSubject())
+			.append("\nBody:\n").append( message.getTextBody() == null ? message.getHtmlBody() : message.getTextBody() )
+			.toString()
+		);
+	}
+
+	private static void logResponse(PostmarkResponse resp) {
+		Logger.info(
+			new StringBuilder()
+			.append("\n------- Postmark response ------")
+			.append("\nStatus: ").append(resp.getStatus())
+			.append("\nStatus message: ").append(resp.getMessage())
+			.append("\nError code: ").append(resp.getErrorCode())
+			.append("\nTo: ").append(resp.getTo())
+			.toString()
+		);
+	}
+}
--- src/play/modules/postmark/Postmark.java
+++ src/play/modules/postmark/Postmark.java
@@ -1,45 +1,51 @@
 package play.modules.postmark;
 
-import com.postmark.java.PostmarkClient;
-import com.postmark.java.PostmarkException;
-import com.postmark.java.PostmarkMessage;
-import com.postmark.java.PostmarkResponse;
-
 import play.Logger;
 import play.Play;
 
+/**
+ * @author Joel Soderstrom
+ *
+ * TODO: Add support for sending mail with HTML content 
+ * TODO: Add support for attachments
+ */
 public class Postmark {
 	
-	private static final String PROP_API_KEY = "play.postmark.apiKey";
-	private static final String PROP_FROM = "play.postmark.from";
+	public static Postmark ctx = null;
+
+	private static final String PROP_API_KEY = "postmark.apiKey";
+	private static final String PROP_FROM = "postmark.from";
 	
-	private static boolean isTest = false;
+	private boolean isTest = false;
 	
 	/**
 	 * API-key needed to authenticate at Postmarkapp.com
 	 * http://developer.postmarkapp.com/developer-build.html#authentication-headers
 	 */
-	private static String apiKey;
+	private final String apiKey;
 
 	/**
 	 * E-mail address that will populate "from" field in mails.
-	 * Note that this address will be overridden if "from" is set in {@link #sendMail(String, String, String)}
+	 * This address will be overridden if "from" is set in 
+	 * {@link #sendMail(String, String, String)}.
+	 * 
+	 * Note that this address must be a valid "sender signature" registered 
+	 * in your account @ postmarkapp.com.
 	 */
-	private static String from;
-	
-	/**
-	 * Client that communicates with Postmark's REST API.
-	 */
-	private static PostmarkClient postmarkClient;
-	
+	private final String defaultFrom;
 	
 	public static void init() {
-		apiKey = Play.configuration.getProperty(PROP_API_KEY);
-		from = Play.configuration.getProperty(PROP_FROM);
+		ctx = new Postmark(
+			Play.configuration.getProperty(PROP_FROM), 
+			Play.configuration.getProperty(PROP_API_KEY)
+		);
+	}
+	
+	private Postmark(String defaultFrom, String apiKey) {
+		this.defaultFrom = defaultFrom;
+		this.apiKey = apiKey;
 
 		if(apiKey != null) {
-			postmarkClient = new PostmarkClient(apiKey);
-			
 			/*
 			 * Key set to POSTMARK_API_TEST indicates that we only test run. 
 			 * In that case enable logging for mail response from server. 
@@ -47,61 +53,22 @@ public class Postmark {
 			 * Source:
 			 * http://developer.postmarkapp.com/developer-build.html#authentication-headers
 			 */
-			isTest = apiKey.equals("POSTMARK_API_TEST");
+			this.isTest = apiKey.equals("POSTMARK_API_TEST");
 		} 
 		else {
 			Logger.warn("No API-key for postmark specified, please set property '%s'", PROP_API_KEY);
 		}
 	}
 	
-    public static void sendMail(String from, String to, String subject, String body) {
-    	sendMail(new PostmarkMessage(from, to, null, null, subject, body, false, null));
-    }
-    
-    public static void sendMail(String to, String subject, String body) {
-    	sendMail(null, to, subject, body);
-    }
-
-    public static void sendMail(PostmarkMessage message) {
-    	if(message.getFromAddress() == null && from != null) {
-    		message.setFromAddress(from);
-    	}
-    	
-    	try {
-    		if(isTest) {
-    			logMessage(message);
-    		}
-    		PostmarkResponse resp = postmarkClient.sendMessage(message);
-    		
-    		if(isTest) {
-    			logResponse(resp);
-    		}
-    	} catch (PostmarkException e) {
-    		Logger.warn("Could not send mail to '%s':", message.getToAddress(), e.getMessage());
-    	}
-    }
-
-	private static void logMessage(PostmarkMessage message) {
-		Logger.info(
-			new StringBuilder()
-			.append("\n------- Postmark will send message ------")
-			.append("\nTo: ").append(message.getToAddress())
-			.append("\nFrom: ").append(message.getFromAddress())
-			.append("\nSubject: ").append(message.getSubject())
-			.append("\nBody:\n").append( message.getTextBody() == null ? message.getHtmlBody() : message.getTextBody() )
-			.toString()
-		);
+	public String getDefaultFrom() {
+		return defaultFrom;
 	}
-
-	private static void logResponse(PostmarkResponse resp) {
-		Logger.info(
-			new StringBuilder()
-			.append("\n------- Postmark response ------")
-			.append("\nStatus: ").append(resp.getStatus())
-			.append("\nStatus message: ").append(resp.getMessage())
-			.append("\nError code: ").append(resp.getErrorCode())
-			.append("\nTo: ").append(resp.getTo())
-			.toString()
-		);
+	
+	public String getApiKey() {
+		return apiKey;
+	}
+	
+	public boolean isTest() {
+		return isTest;
 	}
 }
